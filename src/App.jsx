c546c0708572c3e8ba74723f852e3b8edc61d6fd
Kristjan Lääne <xonproject@gmail.com>
2023-09-20 17:55:52 +0300

    props

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,23 @@ import Title from './components/Title'
 function App() {
   const [showEvents, setShowEvents] = useState(true)
   const [events, setEvents] = useState([
-    {title: "mario's birthday bash", id: 1},
-    {title: "bowser's live stream", id: 2},
-    {title: "race on moo moo farm", id: 3}
+    { title: "mario's birthday bash", id: 1 },
+    { title: "bowser's live stream", id: 2 },
+    { title: 'race on moo moo farm', id: 3 },
   ])
 
   const handleClick = (id) => {
-    setEvents(prevEvents => {
-      return prevEvents.filter(event => id !== event.id)
+    setEvents((prevEvents) => {
+      return prevEvents.filter((event) => id !== event.id)
     })
   }
 
+  const subtitle = 'All the latest news in Marioland'
+
   return (
-    <div className="App">
-      <Title />
-      
+    <div className='App'>
+      <Title title='Events in your area' subtitle={subtitle} />
+
       {showEvents && (
         <div>
           <button onClick={() => setShowEvents(false)}>Hide Events</button>
@@ -30,14 +32,17 @@ function App() {
           <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
       )}
-      {showEvents && events.map((event, index) => (
-        <div key={event.id}>
-          <h2>{index} - {event.title}</h2>
-          <button onClick={() => handleClick(event.id)}>delete event</button>
-        </div>
-      ))}
+      {showEvents &&
+        events.map((event, index) => (
+          <div key={event.id}>
+            <h2>
+              {index} - {event.title}
+            </h2>
+            <button onClick={() => handleClick(event.id)}>delete event</button>
+          </div>
+        ))}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
