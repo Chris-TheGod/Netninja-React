a4ec14ce93f5948457d01f571bb133c1481f3da9
Kristjan Lääne <xonproject@gmail.com>
2023-09-22 16:27:08 +0300

    eventlist component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { useState } from 'react'
 import Title from './components/Title'
 import { Modal } from './components/Modal'
+import { EventList } from './components/EventList'
 
 function App() {
   const [showModal, setShowModal] = useState(false)
@@ -36,15 +37,7 @@ function App() {
           <button onClick={() => setShowEvents(true)}>Show Events</button>
         </div>
       )}
-      {showEvents &&
-        events.map((event, index) => (
-          <div key={event.id}>
-            <h2>
-              {index} - {event.title}
-            </h2>
-            <button onClick={() => handleClick(event.id)}>delete event</button>
-          </div>
-        ))}
+      {showEvents && <EventList events={events} handleClick={handleClick} />}
       {showModal && (
         <Modal handleClose={handleClose}>
           <h2>10% off coupon code!</h2>
---
+++ src/components/EventList.jsx
@@ -0,0 +1,14 @@
+export const EventList = ({ events, handleClick }) => {
+  return (
+    <div>
+      {events.map((event, index) => (
+        <div key={event.id}>
+          <h2>
+            {index} - {event.title}
+          </h2>
+          <button onClick={() => handleClick(event.id)}>delete event</button>
+        </div>
+      ))}
+    </div>
+  )
+}
